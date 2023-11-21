f56c490c9bcc8a13a582d379b3bef6508f0eb567
Alex <al.vaccaro@outlook.com>
2023-11-21 12:12:59 +0100

    Ho caricato esercizi ex1 e temp 1,2,3

---
+++ temp1.js
@@ -0,0 +1,7 @@
+// somma di due numeri
+
+const num1 = 10;
+const num2 = 20;
+const sum = `La somma è ${num1+num2}`;
+
+console.log(sum);
---
+++ temp2.js
@@ -0,0 +1,7 @@
+// Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+let string = `Ciao,
+sono una stringa 
+multilinea`;
+
+console.log(string);
