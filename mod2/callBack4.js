ec3401d5e9b96bd553f19ec49474be588978509a
Alex <al.vaccaro@outlook.com>
2023-11-28 14:36:44 +0100

    solto es 5 callback

---
+++ mod2/callBack4.js
@@ -0,0 +1,19 @@
+/*Crea una funzione chiamata runCallbacks che accetta un array
+ di funzioni di callback come argomento.
+La funzione dovrebbe eseguire ogni callback nell'ordine in cui appaiono nell'array.*/
+
+function runCallbacks(callbacks) {
+    // ...
+}
+function firstCallback() {
+    //...
+}
+function secondCallback() {
+    //...
+}
+
+function thirdCallback() {
+    //...
+}
+const callbackArray = [firstCallback, secondCallback, thirdCallback];
+runCallbacks(callbackArray);
