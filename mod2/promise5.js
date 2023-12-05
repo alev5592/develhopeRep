d70659f4900d42193c6c154c39c5aa7afff205a5
Alex <al.vaccaro@outlook.com>
2023-12-05 15:32:20 +0100

    upload promise5.js

---
+++ mod2/promise5.js
@@ -0,0 +1,34 @@
+/*Convertire la seguente funzione basata su callback in una funzione basata su Promise. 
+
+function callback_BasedFunction(arg1, arg2, callback) {
+    setTimeout(() => {
+        const result = arg1 + arg2;
+        if (result % 2 !== 0) {
+            callback(null, result);
+        } else {
+            callback(new Error('Result is not odd!'), null);
+        }
+    }, 1000);
+}
+
+*/
+
+function promise_BasedFunction(arg1,arg2){
+    const result = arg1 + arg2;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(result %2 !== 0){
+                resolve(result)
+            }
+            else{
+                reject("Result is not odd!");
+            }
+        }, 1000);
+    })
+}
+
+const check = promise_BasedFunction(6,3).then((success) => {
+    console.log(success);
+}).catch((error) => {
+    console.log(error);
+})
